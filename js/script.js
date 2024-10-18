13428248fa867657ea9d178f07835ff12034f6da
DanisEzik <arlonlox@gmail.com>
2024-10-18 22:07:42 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,304 @@
+function del(A,B){
+    return Math.round (A / B);
+}
+
+function osti(A,B,C ){
+    return A - B*C
+}
+
+
+let a = +prompt("Введи число");
+let b = +prompt("Введи число номер два");
+let res = (del(a,b));
+let ost = (osti(a,b,r));
+
+let r = a / b;
+
+
+console.log(res);
+console.log(ost);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(max - min == middle) {
+//     alert("Да")
+// } else alert("Нет")
+
+
+
+
+
+
+//  a = +prompt("Введите трехзначное число"); 
+
+// if (a < 100 || a > 999){
+//     a = +prompt("Введи число в котором ты видишь три цифорки" ); 
+// }
+
+// let b = a % 10;
+// a /= 10;
+// a= Math.floor(a);
+
+// let c = a % 10;
+// a /= 10;
+// a= Math.floor(a);
+
+
+
+
+
+
+
+
+
+
+//let a = 5;//
+//let b = 4;//
+//let c = 3;//
+
+//if(a<b && b<c || a>b && b>c){
+   // alert("Да");//
+//} else alert ("Нет");//
+
+
+
+// let m = 1;
+
+// switch(m){
+
+//     case 1:
+//         alert("Вери плехо >:(")
+//         break;
+
+//     case 2:
+//         alert("Не очень плехо >:|")
+//         break;
+
+//     case 3:
+//         alert("Намально >:)")
+//         break;
+
+//     case 4:
+//         alert("Ты мозещ луцше :)")
+//         break;
+
+//     case 5:
+//         alert("0_0")
+//         break;
+
+//  }
+
+
+//  let a = 100;
+//  let b = 255;
+//  let c = 125;
+
+//  let max = 0;
+//  let min = 0;
+//  let middle = 0;
+//  if (a < b && a < c) {
+//      min = a;
+//      if (b > c) {
+//          max = b;
+//          middle = c;
+//      } else {
+//          max = c;
+//          middle = b;
+//      }
+//  }
+
+
+//  if (c < b && a > c) {
+//      min = c;
+//      if (b > a) {
+//         max = b;
+//          middle = a;
+//    } else {
+//        max = a;
+//        middle = b;
+//    }
+// }
+
+// if (a > b && b < c) {
+//      min = b;
+//    if (a > c) {
+//          max = a;
+//         middle = c;
+//     } else {
+//         max = c;
+//         middle = a;
+//    }
+// }
+
+// alert(max + middle);
+
+
+// !!!ПЕРЕХОД!!!
+
+
+// let a = +prompt("Введите первое число");
+// let b = +prompt("Введите второе число");
+// let c = +prompt("Введите третье число");
+
+//  let max = 0;
+//  let min = 0;
+//  let middle = 0;
+//  if (a < b && a < c) {
+//      min = a;
+//      if (b > c) {
+//          max = b;
+//          middle = c;
+//      } else {
+//          max = c;
+//          middle = b;
+//      }
+//  }
+
+
+//  if (c < b && a > c) {
+//      min = c;
+//      if (b > a) {
+//         max = b;
+//          middle = a;
+//    } else {
+//        max = a;
+//        middle = b;
+//    }
+// }
+
+// if (a > b && b < c) {
+//      min = b;
+//    if (a > c) {
+//          max = a;
+//         middle = c;
+//     } else {
+//         max = c;
+//         middle = a;
+//    }
+// }
+
+// console.log(max);
+// console.log(middle);
+// console.log(min);
+
+
+// !!!ПЕРЕХОД!!!
+
+// let a = prompt("Введи величину");
+// let x = +prompt("Введи число");
+
+   
+// if(a == "Километр"){
+//     alert(x*1000);
+// }else if(a == "Метр"){
+//     alert(x);
+// }else if(a == "Дециметр"){
+//     alert(x*0.1);
+// }else if(a == "Сантиметр"){
+//     alert(x*0.01);
+// }else if(a == "Милиметр"){
+//     alert(x*0.001);
+// };
+
+
+// let a = +prompt("Введи чиселко ")
+// let b = +prompt("Введи чиселко номер 2");
+// let s = 0;
+
+// while (a <= b ){
+//     s = s + a;
+//     a++;
+// }
+// console.log(s);
+
+
+// function step(A, B){
+//     return Math.pow(A, B);
+// }
+
+
+
+
+// let a = +prompt("Введи число");
+// let b = +prompt("Введи число второе");
+// let res = (step(a , b));
+// console.log(res);
