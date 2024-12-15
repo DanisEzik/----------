3c7782ad115844928fbee675637c2038e40e3cc4
DanisEzik <arlonlox@gmail.com>
2024-12-15 14:44:10 +0500

    endproject

--- js/script.js
+++ js/script.js
@@ -1,22 +1,378 @@
-function del(A,B){
-    return Math.round (A / B);
+let pass = document.getElementById("vvod");
+let password = pass.value.split("");
+let error = "Ошибка, ваш пароль подходит не по всем параметрам!";
+let good = "Поздравляем, ваш пароль успешно сохранен!";
+
+
+if(password.length < 8){
+   setTimeout(10).alert(error);
+} else alert(good);
+
+if(password.length > 20){
+   alert(error);
+} else alert(good);
+
+for(let i = 0; i < password.length; i++){
+   if(password[i] != 0){
+      alert(error);
+   } elif(password[i] != 1);{
+      alert(error);
+   } elif(password[i] != 2);{
+      alert(error);
+   } elif(password[i] != 3);{
+      alert(error);
+   } elif(password[i] != 4);{
+      alert(error);
+   } elif(password[i] != 5);{
+      alert(error);
+   } elif(password[i] != 6);{
+      alert(error);
+   } elif(password[i] != 7);{
+      alert(error);
+   } elif(password[i] != 8);{
+      alert(error);
+   } elif(password[i] != 9);{
+      alert(error);
+   } 
+     
 }
 
-function osti(A,B,C ){
-    return A - B*C
+for(let i = 0; i < password.length; i++){
+   if(password[i] != "!");{
+      alert(error);
+   } elif(password[i] != "@");{
+      alert(error);
+   } elif(password[i] != "#");{
+      alert(error);
+   } elif(password[i] != "$");{
+      alert(error);
+   } elif(password[i] != "%");{
+      alert(error);
+   } elif(password[i] != "^");{
+      alert(error);
+   }  elif(password[i] != "&");{
+      alert(error);
+   } elif(password[i] != "*");{
+      alert(error);
+   } elif(password[i] != "(");{
+      alert(error);
+   } elif(password[i] != ")");{
+      alert(error);
+   } elif(password[i] != "-");{
+      alert(error);
+   } elif(password[i] != "=");{
+      alert(error);
+   } elif(password[i] != "_");{
+      alert(error);
+   } elif(password[i] != "=");{
+      alert(error);
+   } elif(password[i] != "/");{
+      alert(error);
+   } elif(password[i] != "|");{
+      alert(error);
+   } elif(password[i] != ">");{
+      alert(error);
+   } elif(password[i] != "<");{
+      alert(error);
+   } elif(password[i] != "[");{
+      alert(error);
+   } elif(password[i] != "]");{
+      alert(error);
+   } elif(password[i] != "{");{
+      alert(error);
+   } elif(password[i] != "}");{
+      alert(error);
+   } elif(password[i] != ";");{
+      alert(error);
+   } elif(password[i] != ":");{
+      alert(error);
+   } elif(password[i] != "`");{
+      alert(error);
+   } elif(password[i] != "~");{
+      alert(error);
+   } elif(password[i] != "№");{
+      alert(error);
+   } elif(password[i] != ".");{
+      alert(error);
+   } elif(password[i] != ",");{
+      alert(error);
+   } elif(password[i] != "?");{
+      alert(error);
+   }  
+}
+
+for(let i = 0; i < password.length; i++){
+   if(password[i] != "q");{
+      alert(error);
+   } elif(password[i] != "w");{
+      alert(error);
+   } elif(password[i] != "e");{
+      alert(error);
+   } elif(password[i] != "r");{
+      alert(error);
+   } elif(password[i] != "t");{
+      alert(error);
+   } elif(password[i] != "y");{
+      alert(error);
+   } elif(password[i] != "u");{
+      alert(error);
+   } elif(password[i] != "i");{
+      alert(error);
+   } elif(password[i] != "o");{
+      alert(error);
+   } elif(password[i] != "p");{
+      alert(error);
+   }  elif(password[i] != "a");{
+      alert(error);
+   } elif(password[i] != "s");{
+      alert(error);
+   } elif(password[i] != "d");{
+      alert(error);
+   } elif(password[i] != "f");{
+      alert(error);
+   } elif(password[i] != "g");{
+      alert(error);
+   } elif(password[i] != "h");{
+      alert(error);
+   } elif(password[i] != "j");{
+      alert(error);
+   } elif(password[i] != "k");{
+      alert(error);
+   } elif(password[i] != "l");{
+      alert(error);
+   } elif(password[i] != "z");{
+      alert(error);
+   } elif(password[i] != "x");{
+      alert(error);
+   } elif(password[i] != "c");{
+      alert(error);
+   } elif(password[i] != "v");{
+      alert(error);
+   } elif(password[i] != "b");{
+      alert(error);
+   } elif(password[i] != "n");{
+      alert(error);
+   } elif(password[i] != "m");{
+      alert(error);
+   } 
+}
+
+for(let i = 0; i < password.length; i++){
+   if(password[i] != "Q");{
+      alert(error);
+   } elif(password[i] != "W");{
+      alert(error);
+   } elif(password[i] != "E");{
+      alert(error);
+   } elif(password[i] != "R");{
+      alert(error);
+   } elif(password[i] != "T");{
+      alert(error);
+   } elif(password[i] != "Y");{
+      alert(error);
+   } elif(password[i] != "U");{
+      alert(error);
+   } elif(password[i] != "I");{
+      alert(error);
+   } elif(password[i] != "O");{
+      alert(error);
+   } elif(password[i] != "P");{
+      alert(error);
+   }  elif(password[i] != "A");{
+      alert(error);
+   } elif(password[i] != "S");{
+      alert(error);
+   } elif(password[i] != "D");{
+      alert(error);
+   } elif(password[i] != "F");{
+      alert(error);
+   } elif(password[i] != "G");{
+      alert(error);
+   } elif(password[i] != "H");{
+      alert(error);
+   } elif(password[i] != "J");{
+      alert(error);
+   } elif(password[i] != "K");{
+      alert(error);
+   } elif(password[i] != "L");{
+      alert(error);
+   } elif(password[i] != "Z");{
+      alert(error);
+   } elif(password[i] != "X");{
+      alert(error);
+   } elif(password[i] != "C");{
+      alert(error);
+   } elif(password[i] != "V");{
+      alert(error);
+   } elif(password[i] != "B");{
+      alert(error);
+   } elif(password[i] != "N");{
+      alert(error);
+   } elif(password[i] != "M");{
+      alert(error);
+   }    
+}
+
+for(let i = 0; i < password.length; i++){
+   if(password[i] != " ");{
+      alert(error);
+   }
 }
 
 
-let a = +prompt("Введи число");
-let b = +prompt("Введи число номер два");
-let res = (del(a,b));
-let ost = (osti(a,b,r));
-
-let r = a / b;
 
 
-console.log(res);
-console.log(ost);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let P = document.getElementById("1");
+
+// let t = document.getElementById("2");
+
+// let l = document.getElementById("3");
+
+// let button = document.querySelector("button");
+
+// let end = 0;
+
+// button.addEventListener("click", function(){
+   
+//    if(P.value == "" || t.value == "" || l.value == ""){
+//    alert("Неккоректные данные");
+//    } else {
+//       end = (P.value * t.value * l.value / 365) / 100;
+//       alert(end);
+//    }
+// })
+
+
+
+
+// let table = document.body.firstElementChild;
+// let tbody = table.firstElementChild;
+// for (let i = 0; i < tbody.children.length; i++){
+//    let element = tbody.children[i];
+//    for (let g = 0; g < element.children.length; g++){
+//       if(g == i){
+//          element.children[g].style.background = "red";
+//       }
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let teams = +prompt("Введи кол-во имен")
+
+// let hobby = new Map();
+
+
+// for(i = 0; i <= teams; i++){
+//    let team = prompt("Введи имя и его кружки через пробел");
+//    let spisok = team.split(" ");
+//    let balli = spisok.length - 1;
+//    hobby.set(spisok[0] , balli);
+// }
+
+// let winner = "";
+// let max = 0;
+
+// hobby.forEach((value, key) => {
+//    if(value > max){
+//       max = value;
+//       winner = key;
+//    }
+// }
+// );
+
+// console.log(winner);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -302,3 +658,629 @@ console.log(ost);
 // let b = +prompt("Введи число второе");
 // let res = (step(a , b));
 // console.log(res);
+
+
+
+
+// function del(A,B){
+//     return Math.round (A / B);
+// }
+
+// function osti(A,B,C ){
+//     return A - B*C
+// }
+
+
+// let a = +prompt("Введи число");
+// let b = +prompt("Введи число номер два");
+// let res = (del(a,b));
+// let ost = (osti(a,b,res));
+
+
+
+
+// console.log(res);
+// console.log(ost);
+
+
+
+
+
+
+
+
+
+// function umn(A,B){
+//     return A * 10 + B;
+// }
+
+
+// let a = +prompt("Введи число А");
+// let b = +prompt("Введи число В");
+
+// let wfdsh = umn(a,b);
+
+// console.log(wfdsh);
+
+
+
+
+
+
+
+// function nemenyai(A) {
+//     let f = 0;
+//     while (A > 0){
+//         f = f * 10;
+//         f = f + A % 10;
+//         A = Math.round(A / 10);
+//     }
+//     return f;
+// }
+
+// let a = +prompt("Введи число");
+
+// let ret = nemenyai(a);
+// console.log(ret);
+
+
+
+// let ovs = function (A, B, C)  {
+//    if (A == 0 || B == 1){
+//       C = " овец";
+//    } else if (A == 1 || A == 2 || A == 3 || A == 4 ){
+//       C = " овца";
+//    } else {
+//       C = " овец";
+//    }
+//    return C;
+// }
+
+// let m = +prompt("Введи число");
+// let a = m % 10;
+// let b = m / 10;
+// let c = "";
+// let l = "На лугу пасётся "
+
+// console.log (l+ m + ovs(a,b,c));
+
+
+
+
+
+
+
+// let ne = function(A,B) {
+//    return  B*10 + A;
+// }  
+// let a = +prompt("Введи число");
+// let b = +prompt("Введи второе число")
+
+// console.log(ne(a,b));
+
+
+
+
+
+
+
+
+// function svost(A){
+//    let dpg = {};
+//    for(let y = 0; y < A ; y++){
+//       let svst = prompt ("Введи свойство");
+//       let znach = prompt("Введи значение");
+//       dpg[svst] = znach; 
+//    }
+//    console.log (dpg);
+//    let key = prompt("Введи ключ");
+//    console.log(dpg[key]);
+// }
+
+
+// let a = +prompt("Сколько свойств?");
+// svost(a);
+
+
+
+
+
+// function zap(A){
+//    let hero = {
+//       name:"",
+//       clas:"",
+//       level:0,
+//       inv:0,
+//    };
+//    let name = prompt ("Введи имя героя");
+//    let clas = prompt("Введи класс героя");
+//    let level = +prompt("Введи уровень героя");
+//    let inv = +prompt("Введи кол-во предметов в инвентаре героя");
+//    hero.name = name;
+//    hero.clas = clas;
+//    hero.level = level;
+//    hero.inv = inv;
+//    console.log (hero);
+//    let key = prompt("Введи +lev(для повышения уровня), -lev(для понижения уровня), +inv(для повышения предметов в инвентаре), -inv(для понижения предметов в инвентаре)");
+//    if (key === "+lev"){
+//       hero.level += 1;
+//    }else if (key === "-lev"){
+//       hero.level -= 1;
+//    }else if (key === "+inv"){
+//       hero.inv += 1;
+//    }else if (key === "-inv") {
+//       hero.inv -= 1;
+//    }else console.log("Нету тако програми");
+//    console.log(hero);
+    
+// }
+
+// zap();
+
+
+
+
+
+
+// function zap(A){
+//    let dgg = {};
+//    for(let y = 0; y < A ; y++){
+//       let name = prompt ("Введи имя");
+//       let tel = prompt("Введи номер телефона(начиная с +7)");
+//       dgg[name] = tel; 
+//    }
+//    console.log (dgg);
+//    let key = prompt("Введи существующее имя");
+//    console.log(dgg[key]);
+// }
+
+
+// let a = +prompt("Сколько записей?");
+// zap(a);
+
+
+
+
+// function  ffhh(){
+//    let color = prompt ("Введи цвет");
+//    let shir = prompt ("Введи ширину");
+//    let vis = prompt ("Введи высогу");
+//    let svost = {
+//       color:color,
+//       size:{
+//          shir:shir,
+//          vis:vis,
+//       } 
+//    }
+//    let weta = {
+//       "red":"orange",
+//       "orange":"yellow",
+//       "yellow":"green",
+//       "green":"cyan",
+//       "cyan":"blue",
+//       "blue":"purple",
+//       "purple":"red",
+//    }  
+//    let ob2 = Object.assign(svost);
+//    ob2.size.shir = 2 * ob2.size.shir;
+//    ob2.size.vis = 2 * ob2.size.vis;
+//    console.log(ob2.size.shir);
+//    console.log(ob2.size.vis);
+//    ob2.color = (weta[color]);
+//    console.log(ob2.color);
+// }
+// let end = ffhh();
+
+
+
+
+
+// let a = +prompt("Введи целое число номер 1");
+// let b = +prompt("Введи целое число номер 2");
+
+// let spisok = [];
+
+
+// for(let i = a; i <= b; i++){
+//    spisok.push(i);
+// }
+
+// console.log(spisok.reverse());
+
+// let summ = 0;
+
+// for(let i = 0; i < spisok.length; i++){
+//    summ = summ + spisok[i];
+// }
+// console.log(summ);
+
+
+
+
+
+
+
+
+// let a = +prompt("Введи первую цифру числа");
+// let b = +prompt("Введи вторую цифру числа");
+// let c = +prompt("Введи последнюю цифру числа");
+
+// let spisok = [];
+// spisok.push(a);
+// spisok.push(b);
+// spisok.push(c);
+// if (spisok[0] == 3){
+//    console.log(1);
+// } else if (spisok[1] == 3){
+//    console.log(2);
+// } else if (spisok[2] == 3){
+//    console.log(3);
+// } else console.log(0);
+
+
+
+
+// let a = +prompt("Введи первую цифру числа");
+// let b = +prompt("Введи вторую цифру числа");
+// let c = +prompt("Введи третью цифру числа");
+// let d = +prompt("Введи последнюю цифру числа");
+
+// let spisok = [];
+
+// spisok.push(a);
+// spisok.push(b);
+// spisok.push(c);
+// spisok.push(d);
+// if (spisok.reverse == spisok){
+//    console.log("Да");
+// }
+
+
+
+
+
+// let a = +prompt("Введи целое число номер 1");
+// let b = +prompt("Введи целое число номер 2");
+
+// let spisok = [];
+// let spsiok2 = [];
+
+// for(let i = a; i <= b; i++){
+//    spisok.push(i);
+// }
+
+
+// for(let i = 0; i < spisok.length; i++){
+//    if (spisok[i] % 2 == 0){
+//       spsiok2.push(spisok[i]);
+//    }
+// }
+// console.log(spsiok2);
+// console.log(spsiok2.length);
+
+
+
+
+
+
+// let a = prompt("Введи последователльность чисел");
+// let n = +prompt("Введи число");
+
+// let spisok = a.split(" ");
+// let spisok2 = []
+// spisok.forEach((item) => {
+//    if (+item < n && +item < spisok.length) {
+//        spisok2.push(+item);
+//    }
+// })
+// console.log(spisok2);
+
+
+
+
+
+
+// let a1 = prompt("Введи последовательность чисел");
+// let a2 = prompt("Введи вторую последовательность чисел");
+
+// let spisok = a1.split(" ");
+// let spisok2 = a2.split(" ")
+// let spisok3 = [];
+
+// spisok.sort(function(a, b) {
+//    if (a < b) return 1;
+//    else if (a > b) return -1;
+//    else return 0;
+// });
+
+// spisok2.sort(function(a, b) {
+//    if (a < b) return 1;
+//    else if (a > b) return -1;
+//    else return 0;
+// });
+
+// spisok3.push(spisok[0]);
+// spisok3.push(spisok2[0]);
+
+// spisok3.sort(function(a, b) {
+//    if (a > b) return 1;
+//    else if (a < b) return -1;
+//    else return 0;
+// });
+// console.log(spisok3);
+
+
+
+
+
+
+
+// let a1 = prompt("Введи последовательность имен");
+// let spisok = a1.split(" ");
+// spisok.sort(function(a, b) {
+//    if (a < b) return 1;
+//    else if (a > b) return -1;
+//    else return 0;
+// });
+// console.log(spisok);
+
+
+
+
+
+// let a1 = prompt("Введи последовательность чисел");
+// let spisok = a1.split(" ");
+// spisok = spisok.map(function(item){
+//    return Number(item);
+// });
+// let b = 0;
+// let a = 0
+
+// for (i = 0; i < spisok.length; i++){
+//    if (spisok[i] % 2 == 1){
+//       a = spisok[i];
+//       break;
+//    }
+// }
+
+// for (i = 0; i < spisok.length; i++){
+//    if (spisok[i] % 2 == 0){
+//       spisok[i] += a;
+//    }
+// }
+
+// console.log(spisok);
+
+
+
+
+
+
+
+
+
+// let teams = +prompt("Введи кол-во команд")
+
+// let igri = new Map();
+
+
+
+
+
+
+// for(i = 0; i <= teams; i++){
+//    let team = prompt("Введи название команды, кол-во побед, ничей и поражений через SPACE");
+//    let spisok = team.split(" ");
+//    let schet = +spisok[1] * 3 + +spisok[2];
+//    igri.set(spisok[0] , schet);
+// }
+
+// let winner = "";
+// let max = 0;
+
+// igri.forEach((value, key) => {
+//    if(value > max){
+//       max = value;
+//       winner = key;
+//    }
+// }
+// );
+
+// console.log(winner);
+
+
+
+
+
+
+
+// let library_card = document.getElementById("library_card");
+// console.log(library_card);
+
+
+// let all = document.querySelectorAll("td")
+// console.log(all[7]);
+
+
+// let inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// console.log(inputs[0]);
+// console.log(inputs[inputs.length - 1]); 
+
+
+// let all = document.querySelectorAll("input, textarea");
+// for(i = 0; i < all.length; i++){
+//    console.log(all[i].getAttribute("name"));
+// }
+
+// let spisok = document.getElementById("solar_system");
+// while(spisok.firstChild){
+//    spisok.firstChild.remove();
+// }
+
+
+// let y = document.createElement("ul");
+// document.body.append(y);
+// while(true){
+//    let element = prompt("Введите пожалуйста, уважаемый пользователь, li");
+//    if(element == null){
+//       break;
+//    }
+//    let t = document.createElement("li");
+//    t.textContent = element;
+//    y.appendChild(t);
+// }
+
+
+// let div = document.createElement("div");
+// let smth = document.createElement("p");
+// smth = prompt("Введите надпись");
+// document.body.append(smth);
+// setTimeout(() => {
+//    setTimeout(() => {
+//      smth.remove();
+//     }, 2); 
+// }, timeout);
+
+
+
+
+
+
+
+
+
+// let color = {
+
+//    "Красный": {
+//        "rgb(255, 0, 0)": {},
+//        "#ff0000": {}
+//    },
+
+//    "Зелёный": {
+//        "rgb(0, 255, 0)": {},
+//        "#00ff00": {}
+//    },
+
+//    "Синий": {
+//        "rgb(0, 0, 255)": {},
+//        "#0000ff": {}
+//    }
+
+// };
+
+// let allcveta = document.createElement("ul");
+
+// for (key in color) {
+
+//     let cvet = document.createElement("li");
+//     cvet.textContent = key;
+
+//     let ul = document.createElement("ul");
+
+//     let map = new Map(Object.entries(color[key]));
+
+//     map.forEach((value, key) => {
+//         let key_color = document.createElement("li");
+//         key_color.textContent = key;
+//         ul.appendChild(key_color);
+//     });
+
+//     cvet.appendChild(ul);
+//     allcveta.appendChild(cvet);
+    
+// }
+
+// document.body.append(allcveta);
+
+
+
+
+
+
+// let liis = document.querySelectorAll("li");
+
+// for(let i = 0; i < liis.length; i++){
+//    liis[i].addEventListener("click", function(event){
+//       if(event.ctrlKey){
+//          liis[i].style.background = "blue";
+//       } else {
+//          for(let i = 0; i < liis.length; i++){
+//             liis[i].style.background = "white";
+//          }
+//          liis[i].style.background = "blue";
+//       }
+
+//    })
+// }
+
+
+
+
+
+
+// let button = document.getElementById("button");
+// let text = document.getElementById("text");
+// let skolko = 0;
+// button.onclick = function(){
+//    skolko += 1;
+//    if(skolko % 2 == 0){
+//       button.style.background = "red";
+      
+//    } else button.style.background = "black";
+//    text.textContent = skolko;
+// }
+
+
+
+
+
+// function getRandomInt(max) {
+//    return Math.floor(Math.random() * max);
+//  }
+ 
+
+
+// let cweta = ["red", "blue", "yellow", "green", "white", "black"];
+// let nadp = document.querySelectorAll("td");
+// let buttons = document.getElementsByClassName("changeColor");
+// buttons[0].onclick = function(){
+//    nadp[0].style.background = cweta[getRandomInt(6)];
+// }
+
+// buttons[1].onclick = function(){
+//    nadp[2].style.background = cweta[getRandomInt(6)];
+// }
+
+// buttons[2].onclick = function(){
+//    nadp[4].style.background = cweta[getRandomInt(6)];
+// }
+
+
+
+// let tasks = [];
+// let buttons = document.getElementById("delete");
+// for(let i = 0; i < spisok.length; i++){
+//    tasks.push(spisok[i]);
+// }
+// for(let i = 0; i < spisok.length; i++){
+//    buttons[i].onclick = function(){
+//       spisok[i].remove;
+//    }
+// }
+
+
+
+
+
+// let input = document.getElementById("text");
+
+// input.onkeydown = input.onkeyup = fun;
+
+// function fun(event) {
+//    let spisok = ["0","1","2","3","4","5","6","7","8","9","+","-","(",")"] 
+//    return (event.key in spisok.includes || (event.ctrlKey && (event.code == "KeyA" || event.code == "KeyC" || event.code == "KeyV" || event.code == "Backspace") ));
+// }
+
+
+
